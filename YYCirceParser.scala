ed00e9fb60e1d4394dc704bc1c3998d746f75642
João Costa <jdpc557@gmail.com>
2025-03-31 23:07:18 +0100

    Reuse string length from tag

--- YYCirceParser.scala
+++ YYCirceParser.scala
@@ -1,18 +1,35 @@
+import io.circe.*
+import java.nio.charset.Charset
 import lib_yyjson.all.*
+import scala.annotation.switch
 import scalanative.libc.string.strlen
+import scalanative.runtime.ffi
 import scalanative.unsafe.*
 import scalanative.unsigned.*
-import io.circe.*
-import scala.annotation.switch
 
 object YYCirceParser extends Parser {
+  private val utf8 = Charset.forName("UTF-8")
+  private def fromStringVal(cstr: Ptr[yyjson_val]): String = {
+    if (cstr == null) {
+      null
+    } else {
+      val len = yyjson_get_len(cstr)
+      val intLen = len.toInt
+      if (intLen > 0) {
+        val bytes = new Array[Byte](intLen)
+        ffi.memcpy(bytes.at(0), yyjson_get_str(cstr), len)
+        new String(bytes, utf8)
+      } else ""
+    }
+  }
+
   private def loop(value: Ptr[yyjson_val])(implicit z: Zone): Json = {
     (yyjson_get_type(value).toInt: @switch) match {
       case 0|1 => throw new RuntimeException("Invalid JSON")
       case 2 => Json.Null
       case 3 => Json.fromBoolean(yyjson_get_bool(value))
       case 4 => Json.fromDouble(yyjson_get_num(value)).get
-      case 5 => Json.fromString(fromCString(yyjson_get_str(value)))
+      case 5 => Json.fromString(fromStringVal(value))
       case 6 => 
         val iter = yyjson_arr_iter_with(value)
         val iterable = Iterator.continually(yyjson_arr_iter_next(iter.toPtr))
@@ -25,7 +42,7 @@ object YYCirceParser extends Parser {
         val iterable = Iterator.continually(yyjson_obj_iter_next(iter.toPtr))
           .takeWhile(_ != null)
           .map { name =>
-            fromCString(yyjson_get_str(name)) -> loop(yyjson_obj_iter_get_val(name))
+            fromStringVal(name) -> loop(yyjson_obj_iter_get_val(name))
           }
           .toMap
         Json.fromJsonObject(JsonObject.fromMap(iterable))
--- YYUJsonParser.scala
+++ YYUJsonParser.scala
@@ -1,18 +1,36 @@
+import io.circe.*
+import java.nio.charset.Charset
 import lib_yyjson.all.*
+import scala.annotation.switch
 import scalanative.libc.string.strlen
+import scalanative.runtime.ffi
 import scalanative.unsafe.*
 import scalanative.unsigned.*
-import io.circe.*
-import scala.annotation.switch
 
 object YYUJsonParser {
+  private val utf8 = Charset.forName("UTF-8")
+  private def fromStringVal(cstr: Ptr[yyjson_val]): String = {
+    if (cstr == null) {
+      null
+    } else {
+      val len = yyjson_get_len(cstr)
+      val intLen = len.toInt
+      if (intLen > 0) {
+        val bytes = new Array[Byte](intLen)
+        ffi.memcpy(bytes.at(0), yyjson_get_str(cstr), len)
+        new String(bytes, utf8)
+      } else ""
+    }
+  }
+
+
   private def loop(value: Ptr[yyjson_val])(implicit z: Zone): ujson.Value = {
     (yyjson_get_type(value).toInt: @switch) match {
       case 0|1 => throw new RuntimeException("Invalid JSON")
       case 2 => ujson.Null
       case 3 => if (yyjson_get_bool(value)) ujson.True else ujson.False
       case 4 => ujson.Num(yyjson_get_num(value))
-      case 5 => ujson.Str(fromCString(yyjson_get_str(value)))
+      case 5 => ujson.Str(fromStringVal(value))
       case 6 => 
         val iter = yyjson_arr_iter_with(value)
         val iterator = Iterator.continually(yyjson_arr_iter_next(iter.toPtr))
@@ -24,7 +42,7 @@ object YYUJsonParser {
         val iterator = Iterator.continually(yyjson_obj_iter_next(iter.toPtr))
           .takeWhile(_ != null)
           .map { name =>
-            fromCString(yyjson_get_str(name)) -> loop(yyjson_obj_iter_get_val(name))
+            fromStringVal(name) -> loop(yyjson_obj_iter_get_val(name))
           }
         ujson.Obj.from(iterator)
       case x => throw new RuntimeException(s"Invalid type: $x")
